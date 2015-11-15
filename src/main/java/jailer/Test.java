a537df694e750ed91a2a08a6147c67a364bb74cf
gomesuit <gomesuit@gmail.com>
2015-11-16 01:49:03 +0900

    sample

--- src/main/java/jailer/Test.java
+++ src/main/java/jailer/Test.java
@@ -1,10 +1,26 @@
 package jailer;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
+
+import static org.apache.zookeeper.ZooDefs.Ids.*;
+import static org.apache.zookeeper.ZooDefs.Perms.*;
+
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+		ZooKeeper zk = new ZooKeeper("192.168.33.11:2181", 3000, null);
+		//List<ACL> acls = new ArrayList<ACL>();
+		//acls.add(new ACL(ALL, ANYONE_ID_UNSAFE));
+		zk.create("/tmp/tmp3", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		zk.close();
 	}
 
 }
