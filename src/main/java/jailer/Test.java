520cef7866d7d9bd424f73b9f3b72ba9bbdcf5d6
gomesuit <gomesuit@gmail.com>
2015-11-16 18:14:57 +0900

    zookeeper sample

--- src/main/java/jailer/Test.java
+++ src/main/java/jailer/Test.java
@@ -19,7 +19,10 @@ public class Test {
 		ZooKeeper zk = new ZooKeeper("192.168.33.11:2181", 3000, null);
 		//List<ACL> acls = new ArrayList<ACL>();
 		//acls.add(new ACL(ALL, ANYONE_ID_UNSAFE));
-		zk.create("/tmp/tmp3", "test".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		zk.create("/tmp", "test".getBytes("UTF-8"), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		byte strByte[] = zk.getData("/tmp", false, null);
+		String result = new String(strByte, "UTF-8");
+		System.out.println(result);
 		zk.close();
 	}
 
