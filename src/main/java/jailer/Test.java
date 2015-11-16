cd997f672c70dd6b428b13f4c045271d254b9bf4
gomesuit <gomesuit@gmail.com>
2015-11-16 18:22:58 +0900

    json

--- src/main/java/jailer/Test.java
+++ src/main/java/jailer/Test.java
@@ -10,6 +10,8 @@ import org.apache.zookeeper.ZooDefs.Ids;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.ACL;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static org.apache.zookeeper.ZooDefs.Ids.*;
 import static org.apache.zookeeper.ZooDefs.Perms.*;
 
@@ -19,10 +21,16 @@ public class Test {
 		ZooKeeper zk = new ZooKeeper("192.168.33.11:2181", 3000, null);
 		//List<ACL> acls = new ArrayList<ACL>();
 		//acls.add(new ACL(ALL, ANYONE_ID_UNSAFE));
-		zk.create("/tmp", "test".getBytes("UTF-8"), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		Hoge hoge = new Hoge(1, "test");
+		ObjectMapper mapper = new ObjectMapper();
+		String json = mapper.writeValueAsString(hoge);
+		
+		zk.create("/tmp", json.getBytes("UTF-8"), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
 		byte strByte[] = zk.getData("/tmp", false, null);
 		String result = new String(strByte, "UTF-8");
-		System.out.println(result);
+		Hoge hoge2 = mapper.readValue(result, Hoge.class);
+		System.out.println(hoge2);
+		
 		zk.close();
 	}
 
