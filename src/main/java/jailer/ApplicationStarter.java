09756f6bd15b6acf4b0462f9a545413d08efed5e
gomesuit <gomesuit@gmail.com>
2016-01-06 00:13:26 +0900

    リファクタリング

--- src/main/java/jailer/ApplicationStarter.java
+++ src/main/java/jailer/ApplicationStarter.java
@@ -2,18 +2,26 @@ package jailer;
 
 import jailer.web.zookeeper.JailerZookeeperCurator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApplicationStarter {
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ApplicationStarter.class, args);
 	}
+
+	@Bean
+	public ZooKeeperProperties getZooKeeperProperties(){
+		return new ZooKeeperProperties();
+	}
 	
 	@Bean
-	public JailerZookeeperCurator JansibleZookeeper(){
-		return new JailerZookeeperCurator("192.168.33.11:2181");
+	@Autowired
+	public JailerZookeeperCurator JansibleZookeeper(ZooKeeperProperties zooKeeperProperties){
+		return new JailerZookeeperCurator(zooKeeperProperties.getConnectionString());
 	}
 }
---
+++ src/main/java/jailer/ZooKeeperProperties.java
@@ -0,0 +1,17 @@
+package jailer;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("zookeeper")
+public class ZooKeeperProperties {
+	private String connectionString;
+
+	public String getConnectionString() {
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) {
+		this.connectionString = connectionString;
+	}
+
+}
